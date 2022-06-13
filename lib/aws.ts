35d5716cefe9e24cfad15d1a09def54fc8c5e994
kaito-nakamura-lvgs <102943566+kaito-nakamura-lvgs@users.noreply.github.com>
2022-06-13 12:03:45 +0900

    docs: add libs

---
+++ lib/aws.ts
@@ -0,0 +1,11 @@
+/**
+ * AWS CDKの使用ライブラリ群
+ */
+
+export { Construct } from 'constructs';
+export { Stack, StackProps } from 'aws-cdk-lib';
+export { aws_ec2 as ec2 } from 'aws-cdk-lib';
+export { aws_route53 as route53 } from 'aws-cdk-lib';
+export { aws_iam as iam } from 'aws-cdk-lib';
+export { aws_s3 as s3} from 'aws-cdk-lib';
+export { aws_rds as rds} from 'aws-cdk-lib';
